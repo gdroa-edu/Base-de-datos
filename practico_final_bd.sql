5b1e57ee57c8f8816f15a73645ca8b3b15a5a626
Dahy678 <57072161+Dahy678@users.noreply.github.com>
2019-10-27 16:20:20 -0300

    Add files via upload

---
+++ practico_final_bd.sql
@@ -0,0 +1,25 @@
+--CREATE DATABASE proyecto
+
+USE proyecto
+
+/*CREATE TABLE detalles_ventas_vencimiento(
+	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	nro_cuota int not null,
+	vencimiento date not null, 
+	monto money not null ,
+	saldo money not null
+
+)
+
+CREATE TABLE empleados(
+	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) NOT  NULL,
+	puesto VARCHAR(25) NOT  NULL)
+
+CREATE TABLE marcas(
+	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) NOT  NULL)
+CREATE TABLE lineas(
+	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) NOT  NULL)*/
+	
